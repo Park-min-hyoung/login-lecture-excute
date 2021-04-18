bacbe035986b53b1fa8669b4567ffa06c5341b14
Park-min-hyoung <qkralswgud1324@naver.com>
2021-04-18 16:00:22 +0900

    MVC의 V(View) 분리하기

--- app.js
+++ app.js
@@ -5,24 +5,11 @@ app.set("views", "./views");
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.send(`
-    <!DOCTYPE html>
-    <html lang="ko">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
-        <body>
-            여기는 루트입니다
-        </body>
-    </html>
-    `);
+    res.render("home/index");
 });
 
 app.get("/login", (req, res) => {
-    res.send("로그인 페이지 입니다.");
+    res.render("home/login");
 });
 
 app.listen(4000, function() {
