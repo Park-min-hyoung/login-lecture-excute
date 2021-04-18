6d89c65df33fc3fd63a8bc2dfbb33b8b4e8bd67f
Park-min-hyoung <qkralswgud1324@naver.com>
2021-04-18 17:03:43 +0900

    소스코드 src 분류

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 // 라우팅
-const home = require("./routes/home");
+const home = require("../routes/home");
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
